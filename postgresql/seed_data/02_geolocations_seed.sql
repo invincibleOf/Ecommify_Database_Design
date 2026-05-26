1f617879fae6162a711d602b8aa38dd5621b898b
camilomora90 <124385241+camilomora90@users.noreply.github.com>
2026-05-25 21:11:38 -0500

    Create 02_geolocations_seed.sql
    seed 02_geolocations_seed.sql

---
+++ postgresql/seed_data/02_geolocations_seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO geolocations (
+    zip_code,
+    city,
+    state,
+    latitude,
+    longitude
+)
+VALUES
+('01001', 'Bogotá', 'Cundinamarca', 4.609710, -74.081750),
+('05001', 'Medellín', 'Antioquia', 6.244203, -75.581215),
+('76001', 'Cali', 'Valle del Cauca', 3.451647, -76.531985),
+('08001', 'Barranquilla', 'Atlántico', 10.968540, -74.781320);
