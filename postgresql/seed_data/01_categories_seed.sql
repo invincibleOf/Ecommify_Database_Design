406aeba37539b990062b22a6a04b12b4d6e092fd
camilomora90 <124385241+camilomora90@users.noreply.github.com>
2026-05-25 21:11:15 -0500

    Create 01_categories_seed.sql
    seed data 01_categories_seed.sql

---
+++ postgresql/seed_data/01_categories_seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO categories (
+    category_name_pt,
+    category_name_en
+)
+VALUES
+('eletronicos', 'electronics'),
+('informatica', 'computers'),
+('moveis', 'furniture'),
+('roupas', 'clothing'),
+('esporte_lazer', 'sports_leisure'),
+('beleza_saude', 'beauty_health');
